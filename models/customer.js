6994797811116cded85bff49c63ea17b5b86fe82
Anjalimaliakkal <anjalimsanjali2@gmail.com>
2024-08-27 21:46:22 +0530

    view all customers completed

--- models/customer.js
+++ models/customer.js
@@ -1,6 +1,10 @@
 const mongoose=require("mongoose")
 const customerSchema=mongoose.Schema(
     {
+        customer_id:{
+            type:String,
+            required:true
+        },
         name:{
             type:String,
             required:true
